384091936800b97eacfe04ffd44244430f109d7b
jh lee <tightend01@naver.com>
2026-01-02 01:58:39 +0100

    chore: Update settings for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? "/leejinhyuk0325.github.io/" : "",
+  basePath: isProd ? "/leejinhyuk0325.github.io" : "",
+  output: "export",
 };
 
 export default nextConfig;
