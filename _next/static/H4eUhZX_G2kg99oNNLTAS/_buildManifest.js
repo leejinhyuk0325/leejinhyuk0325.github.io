cc948f3d3fe61922966c65255a628daa4c01ae72
jh lee <tightend01@naver.com>
2026-01-02 02:22:22 +0100

    Updates

--- _next/static/H4eUhZX_G2kg99oNNLTAS/_buildManifest.js
+++ _next/static/H4eUhZX_G2kg99oNNLTAS/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/leejinhyuk0325.github.io//_next/:path+",
-        "destination": "/leejinhyuk0325.github.io/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
