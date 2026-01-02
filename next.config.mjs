d65da47d9b3442af1edf393f06480eb7467d7b13
jh lee <tightend01@naver.com>
2026-01-02 03:04:52 +0100

    chore: Change settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "",
   images: {
     unoptimized: true,
   },
-  assetPrefix: isProd ? "/leejinhyuk0325.github.io/" : "",
-  basePath: isProd ? "/leejinhyuk0325.github.io" : "",
   output: "export",
 };
 
