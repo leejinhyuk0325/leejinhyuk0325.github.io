5ac503ccad21028243f0b178e4c864350b32f910
jh lee <tightend01@naver.com>
2026-01-12 03:46:24 +0100

    커뮤니티 디테일 페이지 하드코딩으로 수정

--- src/app/community/[id]/page.js
+++ src/app/community/[id]/page.js
@@ -1,9 +1,9 @@
-import { getAllCommunityPostIds } from "@/utils/community";
 import CommunityPostContent from "./CommunityPostContent";
 
 export async function generateStaticParams() {
-  const postIds = await getAllCommunityPostIds();
-  return postIds.map((id) => ({
+  // 1부터 1000까지의 ID를 미리 생성
+  const ids = Array.from({ length: 1000 }, (_, i) => i + 1);
+  return ids.map((id) => ({
     id: id.toString(),
   }));
 }
