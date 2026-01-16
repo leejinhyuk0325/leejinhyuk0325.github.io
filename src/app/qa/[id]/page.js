c45db6bb1f874179acd6c9f1afcec47508ff63e8
jh lee <tightend01@naver.com>
2026-01-16 03:58:49 +0100

    qa 디테일 페이지 문제 수정

--- src/app/qa/[id]/page.js
+++ src/app/qa/[id]/page.js
@@ -1,5 +1,13 @@
 import QAQuestionContent from "./QAQuestionContent";
 
+export async function generateStaticParams() {
+  // 1부터 50까지의 ID를 미리 생성
+  const ids = Array.from({ length: 100 }, (_, i) => i + 1);
+  return ids.map((id) => ({
+    id: id.toString(),
+  }));
+}
+
 export default function QAQuestionPage() {
   return <QAQuestionContent />;
 }
